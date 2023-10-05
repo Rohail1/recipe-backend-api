dc499eb2b7df1e766c46c683e3a7b08e462d0fa7
Rohail Najam <rohail.najam@oxit.com>
2023-10-05 18:50:01 +0500

    Added basic tests

---
+++ app/app/tests.py
@@ -0,0 +1,24 @@
+"""
+Sample Tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+    """Test the calc Module. """
+
+    def test_add_numbers(self):
+        """Test Adding Numbers together."""
+        res = calc.add(5,6)
+
+        self.assertEqual(res, 11)
+
+
+    def test_subtract_numbers(self):
+        """Test Subtract Numbers together."""
+        res = calc.subtract(10, 15)
+
+        self.assertEqual(res, 5)
