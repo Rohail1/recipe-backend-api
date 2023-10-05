7c2bd36478b0da08bfe295d6046ff57242015130
Rohail Najam <rohail.najam@oxit.com>
2023-10-05 20:28:08 +0500

    Added Command for Wait for DB.

--- app/app/tests.py
+++ app/app/tests.py
@@ -16,7 +16,6 @@ class CalcTests(SimpleTestCase):
 
         self.assertEqual(res, 11)
 
-
     def test_subtract_numbers(self):
         """Test Subtract Numbers together."""
         res = calc.subtract(10, 15)
