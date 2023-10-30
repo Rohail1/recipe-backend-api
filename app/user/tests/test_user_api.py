fc2fc10dec97bbc90d967f2ff8be190835c10cf9
Rohail Najam <rohail_najam@hotmail.com>
2023-10-30 21:01:21 +0500

    Added Recipe Application

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -153,4 +153,3 @@ class PrivateUserApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data['name'], payload['name'])
         self.assertTrue(self.user.check_password(payload['password']))
-
